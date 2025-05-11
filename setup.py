35a4169e91d3c47b8bed390b5a8c75894b470bbf
HectorTablero <hector.tablerodiaz@gmail.com>
2025-05-11 20:36:38 +0200

    Updated readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='diffusion_image_gen',
-    version='0.4.0',
+    version='0.4.1',
     author="Héctor Tablero Díaz, Álvaro Martínez Gamo",
     author_email="hector.tablerodiaz@gmail.com",
     packages=find_packages(),
